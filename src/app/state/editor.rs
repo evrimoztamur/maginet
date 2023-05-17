b59d0de655e1a85c415664a4444299b061e09457
Evrim Öztamur <evrimoztamur@gmail.com>
2023-05-17 10:06:57 +0200

    Place buttons for the editor interface

--- src/app/state/editor.rs
+++ src/app/state/editor.rs
@@ -1,4 +1,4 @@
-use shared::{Board, LobbySettings, LobbySort, DEFAULT_BOARD_SIZE};
+use shared::{Board, LobbySettings, LobbySort, Position, DEFAULT_BOARD_SIZE, Mage};
 use wasm_bindgen::JsValue;
 use web_sys::{CanvasRenderingContext2d, HtmlImageElement};
 
@@ -6,32 +6,176 @@ use super::{LobbyState, MenuState, State};
 use crate::{
     app::{
         Alignment, AppContext, ButtonClass, ButtonElement, ButtonTrim, Interface, Particle,
-        ParticleSort, StateSort, UIElement, UIEvent, BOARD_OFFSET, BOARD_OFFSET_F64, BOARD_SCALE,
-        BOARD_SCALE_F64,
+        ParticleSort, StateSort, ToggleButtonElement, UIElement, UIEvent, BOARD_OFFSET,
+        BOARD_OFFSET_F64, BOARD_SCALE, BOARD_SCALE_F64,
     },
     draw::{draw_crosshair, draw_particle, draw_sprite},
 };
+
+pub enum EditorSelection {
+    Mage(Mage),
+    Tile(Position),
+    None,
+}
 
 pub struct EditorState {
     interface: Interface,
     board: Board,
     particles: Vec<Particle>,
+    selection: EditorSelection,
 }
 
-const BUTTON_MODE_TOGGLE: usize = 0;
+const BUTTON_MENU: usize = 0;
+const BUTTON_MODE_TOGGLE: usize = 10;
 
 impl EditorState {
     pub fn new() -> EditorState {
+        let button_menu = ToggleButtonElement::new(
+            (-60, 118),
+            (20, 20),
+            BUTTON_MENU,
+            ButtonTrim::Round,
+            ButtonClass::Bright,
+            crate::app::ContentElement::Sprite((112, 32), (16, 16)),
+        );
+
         let button_mode_toggle = ButtonElement::new(
-            (16, 188),
+            (236, 228),
             (80, 24),
             BUTTON_MODE_TOGGLE,
             ButtonTrim::Glorious,
             ButtonClass::Action,
-            crate::app::ContentElement::Text("Teleport".to_string(), Alignment::Center),
+            crate::app::ContentElement::Text("Battle".to_string(), Alignment::Center),
         );
 
-        let root_element = Interface::new(vec![Box::new(button_mode_toggle)]);
+        let button_save = ButtonElement::new(
+            (244, 204),
+            (64, 16),
+            BUTTON_MODE_TOGGLE,
+            ButtonTrim::Round,
+            ButtonClass::Default,
+            crate::app::ContentElement::Text("Save".to_string(), Alignment::Center),
+        );
+
+        let button_width_minus = ButtonElement::new(
+            (82, 248),
+            (12, 12),
+            BUTTON_MODE_TOGGLE,
+            ButtonTrim::Round,
+            ButtonClass::Default,
+            crate::app::ContentElement::Sprite((88, 24), (8, 8)),
+        );
+
+        let button_width_plus = ButtonElement::new(
+            (98, 248),
+            (12, 12),
+            BUTTON_MODE_TOGGLE,
+            ButtonTrim::Round,
+            ButtonClass::Default,
+            crate::app::ContentElement::Sprite((80, 24), (8, 8)),
+        );
+
+        let button_height_minus = ButtonElement::new(
+            (216, 114),
+            (12, 12),
+            BUTTON_MODE_TOGGLE,
+            ButtonTrim::Round,
+            ButtonClass::Default,
+            crate::app::ContentElement::Sprite((88, 24), (8, 8)),
+        );
+
+        let button_height_plus = ButtonElement::new(
+            (216, 130),
+            (12, 12),
+            BUTTON_MODE_TOGGLE,
+            ButtonTrim::Round,
+            ButtonClass::Default,
+            crate::app::ContentElement::Sprite((80, 24), (8, 8)),
+        );
+
+        let button_team_left = ButtonElement::new(
+            (236, 114 - 92),
+            (12, 20),
+            BUTTON_MODE_TOGGLE,
+            ButtonTrim::Round,
+            ButtonClass::Default,
+            crate::app::ContentElement::Sprite((64, 24), (8, 8)),
+        );
+
+        let button_team_right = ButtonElement::new(
+            (304, 114 - 92),
+            (12, 20),
+            BUTTON_MODE_TOGGLE,
+            ButtonTrim::Round,
+            ButtonClass::Default,
+            crate::app::ContentElement::Sprite((72, 24), (8, 8)),
+        );
+
+        let button_spell_left = ButtonElement::new(
+            (244, 114 - 44),
+            (12, 20),
+            BUTTON_MODE_TOGGLE,
+            ButtonTrim::Round,
+            ButtonClass::Default,
+            crate::app::ContentElement::Sprite((64, 24), (8, 8)),
+        );
+
+        let button_spell_right = ButtonElement::new(
+            (296, 114 - 44),
+            (12, 20),
+            BUTTON_MODE_TOGGLE,
+            ButtonTrim::Round,
+            ButtonClass::Default,
+            crate::app::ContentElement::Sprite((72, 24), (8, 8)),
+        );
+
+        let button_mana_left = ButtonElement::new(
+            (244, 114 - 8),
+            (12, 12),
+            BUTTON_MODE_TOGGLE,
+            ButtonTrim::Round,
+            ButtonClass::Default,
+            crate::app::ContentElement::Sprite((64, 24), (8, 8)),
+        );
+
+        let button_mana_right = ButtonElement::new(
+            (296, 114 - 8),
+            (12, 12),
+            BUTTON_MODE_TOGGLE,
+            ButtonTrim::Round,
+            ButtonClass::Default,
+            crate::app::ContentElement::Sprite((72, 24), (8, 8)),
+        );
+
+        let button_delete = ButtonElement::new(
+            (260, 138),
+            (32, 20),
+            BUTTON_MODE_TOGGLE,
+            ButtonTrim::Round,
+            ButtonClass::Default,
+            crate::app::ContentElement::Sprite((128, 32), (16, 16)),
+        );
+
+        let mage_interface = Interface::new(vec![
+            Box::new(button_team_left),
+            Box::new(button_team_right),
+            Box::new(button_spell_left),
+            Box::new(button_spell_right),
+            Box::new(button_mana_left),
+            Box::new(button_mana_right),
+            Box::new(button_delete),
+        ]);
+
+        let root_element = Interface::new(vec![
+            Box::new(button_menu),
+            Box::new(button_mode_toggle),
+            Box::new(button_save),
+            Box::new(button_width_minus),
+            Box::new(button_width_plus),
+            Box::new(button_height_minus),
+            Box::new(button_height_plus),
+            Box::new(mage_interface),
+        ]);
 
         let board = Board::new(DEFAULT_BOARD_SIZE.0, DEFAULT_BOARD_SIZE.1).unwrap();
 
@@ -39,6 +183,7 @@ impl EditorState {
             interface: root_element,
             board,
             particles: Vec::new(),
+            selection: EditorSelection::None
         }
     }
 }
@@ -56,12 +201,9 @@ impl State for EditorState {
 
         context.save();
 
-        context.translate(BOARD_OFFSET_F64.0 + 64.0, BOARD_OFFSET_F64.1 + 48.0)?;
+        context.translate(BOARD_OFFSET_F64.0 - 32.0, BOARD_OFFSET_F64.1)?;
 
-        draw_sprite(context, atlas, 256.0, 0.0, 64.0, 64.0, 0.0, 0.0)?;
-        draw_sprite(context, atlas, 448.0, 0.0, 64.0, 64.0, 64.0, 0.0)?;
-        draw_sprite(context, atlas, 256.0, 192.0, 64.0, 64.0, 0.0, 64.0)?;
-        draw_sprite(context, atlas, 448.0, 192.0, 64.0, 64.0, 64.0, 64.0)?;
+        draw_sprite(context, atlas, 256.0, 0.0, 256.0, 256.0, 0.0, 0.0)?;
 
         for particle in self.particles.iter_mut() {
             particle.tick();
@@ -72,7 +214,7 @@ impl State for EditorState {
 
         if let Some(selected_tile) = self.board.location_as_position(
             pointer.location,
-            (BOARD_OFFSET.0 + 64, BOARD_OFFSET.1 + 48),
+            (BOARD_OFFSET.0 - 32, BOARD_OFFSET.1),
             BOARD_SCALE,
         ) {
             draw_crosshair(context, atlas, &selected_tile, (32.0, 32.0), frame)?;
@@ -91,7 +233,7 @@ impl State for EditorState {
 
         if let Some(selected_tile) = self.board.location_as_position(
             pointer.location,
-            (BOARD_OFFSET.0 + 64, BOARD_OFFSET.1 + 48),
+            (BOARD_OFFSET.0 - 32, BOARD_OFFSET.1),
             BOARD_SCALE,
         ) {
             if pointer.clicked() {
